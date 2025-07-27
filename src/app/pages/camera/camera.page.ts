9da38d5e2a21d236072a66ae102d6043a6906b4e
khun ly <khun.ly@bstorm.be>
2025-07-27 12:24:28 +0200

    add loader

--- src/app/pages/camera/camera.page.ts
+++ src/app/pages/camera/camera.page.ts
@@ -1,7 +1,7 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton } from '@ionic/angular/standalone';
+import { LoadingController, IonContent, IonHeader, IonTitle, IonToolbar, IonButton } from '@ionic/angular/standalone';
 import { Camera, CameraResultType } from '@capacitor/camera';
 import { CompletionService } from 'src/app/services/completion.service';
 import { switchMap, tap } from 'rxjs';
@@ -16,6 +16,7 @@ import { switchMap, tap } from 'rxjs';
 export class CameraPage implements OnInit {
 
   private readonly completionService = inject(CompletionService);
+  private readonly loadingCtrl = inject(LoadingController); 
 
   constructor() { }
 
@@ -28,10 +29,15 @@ export class CameraPage implements OnInit {
       resultType: CameraResultType.Base64 
     })
     if(result.base64String) {
+      const loader = await this.loadingCtrl.create({ 
+        message: 'Traitement en cours'
+      });
+      loader.present();
       this.completionService.getDenomination(result.base64String).pipe(
         tap(console.log),
         switchMap((result: any) => this.completionService.getEan(JSON.parse(result.choices[0].message.content).denomination))
       ).subscribe((result: any) => {
+        loader.dismiss();
         console.log(console.log(JSON.parse(result.choices[0].message.content)))
       })
     }
